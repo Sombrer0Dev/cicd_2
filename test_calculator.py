30a73a07eca615e7d76a3758b7dc9d3b35003de3
Artem Sokolov <arsokolov@ptsecurity.com>
2025-03-22 20:08:39 +0300

    fix

--- test_calculator.py
+++ test_calculator.py
@@ -2,6 +2,6 @@ from calculator import add
 
 
 def test_add():
-    assert add(2 + 3) == 5, "Сложение не работает"
+    assert add(2, 3) == 5, "Сложение не работает"
     assert add(-1, 1) == 0, "Сложение не работает"
     assert add(0, 0) == 0, "Сложение не работает"
